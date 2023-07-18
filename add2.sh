7537b959dfb067d00b4ce1d2c0189f2391d07ac4
lisadw1 <136782676+lisadw1@users.noreply.github.com>
2023-07-18 11:30:41 +0800

    Create add2.sh

---
+++ add2.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+LOCAL_DIR="/kaggle/working/dddd_trainer/projects/ocr1/models"
+WEBDAV_URL="https://wani.teracloud.jp/dav"
+USERNAME="dawda"
+PASSWORD="DpS5H3jzA9VxF8hr"
+
+# 创建当前时间戳文件夹名称
+TIMESTAMP=$(date +"%Y%m%d-%H%M%S")
+REMOTE_DIR="${WEBDAV_URL}/${TIMESTAMP}"
+
+# 使用MKCOL方法创建WebDAV远程文件夹
+curl -X MKCOL -u "${USERNAME}:${PASSWORD}" "${REMOTE_DIR}"
+
+for file in "${LOCAL_DIR}"/*; do
+  if [ -f "$file" ]; then
+    remote_file="${REMOTE_DIR}/$(basename "${file}")"
+    curl -T "${file}" -u "${USERNAME}:${PASSWORD}" "${remote_file}"
+  fi
+done
+
+# 上传ocr1.zip到WebDAV服务器的时间戳文件夹中
+ocr1_zip="/kaggle/working/captcha/ocr1.zip"
+remote_zip="${REMOTE_DIR}/ocr1.zip"
+curl -T "${ocr1_zip}" -u "${USERNAME}:${PASSWORD}" "${remote_zip}"
